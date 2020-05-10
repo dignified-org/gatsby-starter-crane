a5f8a07b698fea338478cb6ee71f7090e993dfe5
Quinn Blenkinsop <quinn@qw-in.com>
2020-05-10 12:51:14 -0700

    Update starter to look good

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Crane Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/templates/product.js
+++ src/templates/product.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { FormattedNumber } from 'react-intl';
+import Layout from '../components/layout';
 
-const ProductPage = ({ data }) => {
+function ProductPage({ data }) {
+  console.log(data);
+
   const product = data.shopifyProduct
+
+  const { minVariantPrice, maxVariantPrice } = product.priceRange;
+  const images = product.images.edges.map((edge) => edge.node);
+
   return (
-    <pre>
-      {JSON.stringify(product, null, 2)}
-    </pre>
-  )
+    <Layout>
+      <div className="container mx-auto bg-white py-8 border-t border-gray-400">
+        <div className="grid grid-cols-3 gap-8">
+          <div className="col-span-2">
+            {
+              images.map((img) => (
+                <div key={img.id} className="bg-white border rounded-lg overflow-hidden">
+                  <div className="relative pb-2/3">
+                    <img className="absolute h-full w-full object-cover" src={img.originalSrc} alt={img.altText} />
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+
+
+          <div className="">
+            <h2 className="text-5xl">{product.title}</h2>
+            <p className="text-xl">
+              {minVariantPrice.amount !== maxVariantPrice.amount && 'From '}
+              <FormattedNumber
+                value={Number.parseFloat(minVariantPrice.amount)}
+                style="currency"
+                currency={minVariantPrice.currencyCode}
+                />
+            </p>
+            <p>{product.description}</p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export const query = graphql`
@@ -16,6 +52,25 @@ export const query = graphql`
       id
       title
       handle
+      description
+      priceRange {
+        minVariantPrice {
+          amount
+          currencyCode
+        }
+        maxVariantPrice {
+          amount
+          currencyCode
+        }
+      }
+      images {
+        edges {
+          node {
+            id
+            originalSrc
+          }
+        }
+      }
     }
   }
 `
